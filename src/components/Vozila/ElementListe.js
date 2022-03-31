4c70521e862c83277310982a5d5efa592d063a6e
MarkoNikolicVIser <markonikolicviser@gmail.com>
2022-03-31 23:37:46 +0200

    privremeno resavanje baga sa pucanjem aplikacije zakomentarisane su opcije prikaza brzine i prikaza satelita

--- src/components/Vozila/ElementListe.js
+++ src/components/Vozila/ElementListe.js
@@ -107,12 +107,12 @@ const ElementListe = ({ vozila, setVozila, vozilo, bazaInfo }) => {
 
     callback(
       prev =>
-        (prev = {
-          x: vozilo.raw.getPosition().x,
-          y: vozilo.raw.getPosition().y,
-          ikonica: vozilo.raw.getIconUrl(),
-          tablice: `${vozilo.name} "${lokacija2 ? lokacija2 : '...'}"`,
-        })
+      (prev = {
+        x: vozilo.raw.getPosition().x,
+        y: vozilo.raw.getPosition().y,
+        ikonica: vozilo.raw.getIconUrl(),
+        tablice: `${vozilo.name} "${lokacija2 ? lokacija2 : '...'}"`,
+      })
     );
   };
   return (
@@ -162,7 +162,7 @@ const ElementListe = ({ vozila, setVozila, vozilo, bazaInfo }) => {
                 marginLeft: '3px',
               }}
             >
-              {vozilo.raw.$$user_position.sc !== 255 &&
+              {/* {vozilo.raw.$$user_position.sc !== 255 ||
               vozilo.raw.$$user_position.sc ? (
                 <svg
                   style={{ fill: 'green', width: '17px' }}
@@ -183,7 +183,7 @@ const ElementListe = ({ vozila, setVozila, vozilo, bazaInfo }) => {
                 >
                   <path d='M8.213 16.984c.97-1.028 2.308-1.664 3.787-1.664s2.817.636 3.787 1.664l-3.787 4.016-3.787-4.016zm-1.747-1.854c1.417-1.502 3.373-2.431 5.534-2.431s4.118.929 5.534 2.431l2.33-2.472c-2.012-2.134-4.793-3.454-7.864-3.454s-5.852 1.32-7.864 3.455l2.33 2.471zm-4.078-4.325c2.46-2.609 5.859-4.222 9.612-4.222s7.152 1.613 9.612 4.222l2.388-2.533c-3.071-3.257-7.313-5.272-12-5.272s-8.929 2.015-12 5.272l2.388 2.533z' />
                 </svg>
-              )}
+              )} */}
               {Senzor('Kontakt') > 0 ? (
                 <svg
                   style={{ width: '17px', fill: 'green' }}
@@ -246,7 +246,8 @@ const ElementListe = ({ vozila, setVozila, vozilo, bazaInfo }) => {
                 {Senzor('Brzina') != 'N/A' ? (
                   <h5>{(Senzor('Brzina') * 10) / 10} km/h</h5>
                 ) : (
-                  <h5>{vozilo.raw.$$user_position.s.toFixed(0)} km/h</h5>
+                  // <h5>{vozilo.raw.$$user_position.s?vozilo.raw.$$user_position.s.toFixed(0):'...'} km/h</h5>
+                  <h5>...</h5>
                 )}
               </div>
             </div>
